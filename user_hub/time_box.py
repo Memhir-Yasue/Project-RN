cbca584741a4681481c4cc1680e710f56d607492
Eyasu Woldu <eyasuwoldu@38-f9-d3-98-8b-67.wireless.temple.edu>
2019-11-02 16:44:56 -0400

    time_stamp -> subreddit added

--- user_hub/time_box.py
+++ user_hub/time_box.py
@@ -2,3 +2,4 @@ def summarize_hours_stats(time_info: dict):
     time_info_sorted = (dict(sorted(time_info.items())))
     for k, v in time_info_sorted.items():
         print(f"{k}: {len(v)}")
+    return time_info_sorted
